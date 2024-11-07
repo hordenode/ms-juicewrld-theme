c4cb7bedd009ea99cb47cdd774d4180d89b14725
hordenode <jarvislolbro@gmail.com>
2024-11-07 16:30:56 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ main() {
 clear
     cd /Applications/MacSploit.app/Contents/Resources/CodeViewer_CodeViewer.bundle/Contents/Resources/ace.bundle
     echo -e "juice wrld install."
-    curl "https://raw.githubusercontent.com/hordenode/ms-juicewrld-theme/main/juice.js" -o "./theme-tomorrow_night.js"
+    curl "https://raw.githubusercontent.com/hordenode/ms-juicewrld-theme/refs/heads/main/juice.js" -o "./theme-tomorrow_night.js"
 }
 
 print_ascii_art() {
